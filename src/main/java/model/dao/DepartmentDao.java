1a9b7c8867b597cc7379ef7dda339e83657a3f83
Thiago Pereira <thiagopnascimento11@gmail.com>
2025-03-19 11:05:06 -0300

    Entities created

--- src/main/java/model/dao/DepartmentDao.java
+++ src/main/java/model/dao/DepartmentDao.java
@@ -1,4 +1,15 @@
-package model.entities.model.dao;
+package model.dao;
 
-public class DepartmentDao {
+import model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+
 }
